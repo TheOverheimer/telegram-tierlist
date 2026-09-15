3f20f0655268b7a2d99d4545498339206c09953c
TheOverheimer <cheler.ender.vobla@gmail.com>
2026-09-15 23:27:04 +0400

    Delete config.py

--- config.py
+++
@@ -1,5 +0,0 @@
-API_ID = 38228396
-API_HASH = "9e255794a0101bb6a8cec28972a4647d"
-PHONE = 79512190750
-CHAT_USERNAME = "@DigRock_Chat"
-PASSWORD = "Timur0550"  # Ваш пароль двухфакторной аутентификации
